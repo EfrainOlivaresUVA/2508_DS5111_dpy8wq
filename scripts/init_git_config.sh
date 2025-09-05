e7bb8a4fb7a5cde3ef95720319d03afe19899d87
EfrainOlivaresUVA <efrainolivaresUVA@gmail.com>
2025-09-05 22:39:42 +0000

    Saving our two init files

---
+++ scripts/init_git_config.sh
@@ -0,0 +1,12 @@
+!#/usr/bin/bash
+
+USER=efrainolivaresUVA@gmail.com
+NAME=EfrainOlivaresUVA
+
+git config --global --list
+
+git config --global user.email ${USER}
+git config --global user.name ${NAME}
+
+git config --global --list
+
